aab4af3b3700c5936b6e7ba0b3033328e7333813
Sudeepta <sudeepsarmah@gmail.com>
2023-10-07 22:39:20 +0530

    Made a new folder named switch

---
+++ switch/switch.py
@@ -0,0 +1,16 @@
+import csv
+
+
+with open("organizations-100.csv", "r") as file:
+    reader = csv.DictReader(file)
+    counts = {}
+    for row in reader:
+        favorite = row["Founded"]
+        if favorite in counts:
+            counts[favorite] += 1
+        else:
+            counts[favorite] = 1
+
+
+for favorite in sorted(counts):
+    print(f"{favorite}: {counts[favorite]}")
